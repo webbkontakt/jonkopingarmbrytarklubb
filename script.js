21069f45f55d67b0d99ae39caf61280613149695
IsacDoesWeb <dahlberg.isac@gmail.com>
2025-05-30 22:26:05 +0200

    Update

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function toggleMeny() {
     const meny = document.getElementById("meny");
     meny.classList.toggle("visad");
 }
+window.addEventListener('scroll', function() {
+    const meny = document.getElementById('meny');
+    if (meny.classList.contains('visad')) {
+        meny.classList.remove('visad');
+    }
+});
